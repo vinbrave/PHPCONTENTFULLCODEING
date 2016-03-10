67a018d736c539fb9f042d591ef1087a27686124
Sun Kang <sunkang@wstaichi.com>
2016-03-10 22:53:29 +0800

    remove vendor

--- vendor/mawelous/yamop/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## 0.2.1 - 2013-11-27
-    + multiple connections
-    + numeric arrays output format
--- vendor/mawelous/yamop/tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$loader = require __DIR__.'/../vendor/autoload.php';
-$loader->add('Mawelous\Yamop\Tests', __DIR__ );
-$loader->add('Model', __DIR__ );
-$loader->add('Mapper', __DIR__ );
